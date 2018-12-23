80f27c910ac33295b8ec5edcb28210678bf65a8f
shaggywolfhound <hannafordrob@gmail.com>
2018-12-23 11:59:21 +0000

    Add mobile navigation and non website assets for future use.

--- js/custom.js
+++ js/custom.js
@@ -4,13 +4,14 @@ $(function(){
     $('#header').load('./header/header.html');
     $('#content').load('./pages/homepage.html');
 });
-
 //todo convert to promise
 setTimeout(function(){
 
     $('#nav li.home').addClass('current');
 
     $('#nav li').on('click', function () {
+        //todo add click on logo
+        console.log('menu');
         let navClass = $(this).attr('class');
         let content = $('#content');
 
@@ -37,8 +38,39 @@ setTimeout(function(){
                     content.load('./pages/homepage.html');
             }
             content.removeClass('fadeOutDown');
-            content.addClass('fadeInUp')
+            content.addClass('fadeInUp');
         },100);
     });
 
+    /*mobile navigation*/
+    let navButton = $('#mobile-nav');
+    let nav = $('#nav ul');
+
+    if ($(window).width() <= 768) {
+        nav.hide();
+    }
+
+    navButton.on('click', function () {
+        //only apply to mobile view
+        if ($(window).width() <= 768) {
+            if(nav.is(":hidden")){
+                console.log('hidden');
+                nav.slideDown();
+            }else{
+                console.log('showing');
+                nav.slideUp();
+            }
+        }
+    });
+
 },200);
+
+// apply styles if re-sized/rotated
+$( window ).resize(function() {
+
+    if ($(window).width() <= 768) {
+        $('#nav ul').hide();
+    }else{
+        $('#nav ul').show();
+    }
+});
